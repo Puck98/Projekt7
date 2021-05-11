e406500d9269bcd883712a9d19c4de5d94e71028
Jacob Westberg <jawes70@gmail.com>
2021-05-11 11:46:19 +0200

    Fixat konstig commit?

---
+++ projekt.m
@@ -0,0 +1,18 @@
+load('Bakad_Data.mat')
+function [D1,D1c,D2,D2c] = select(features, class)
+
+    vect = randsample(150, 75);
+    D1 = [];
+    D1c = [];
+    D2 = [];
+    D2c = [];
+    for i = 1: 150;
+        if(ismember(i, vect))
+            D1(end + 1, :) = features(i, :);
+            D1c(end + 1, :) = [class(i) i];
+        else
+            D2(end + 1, :) = features(i, :);
+            D2c(end + 1, :) = [class(i) i];
+        end
+    end
+end
