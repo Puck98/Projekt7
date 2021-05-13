e05254694b495e80c4f9edd8558ddb523de4da00
Jacob Westberg <jawes70@gmail.com>
2021-05-13 12:30:39 +0200

    Strukturerat upp klassifieringen av data och testar den

--- projekt.m
+++ projekt.m
@@ -2,9 +2,33 @@ load('Bakad_Data.mat')
 
 [cov1,cov2,cov3] = kovarians(D1,D1c);
 [my1,my2,my3] = vantevarde(D1, D1c);
+[p1,p2,p3] = pvarden(D1,D1c);
 
-pdf1 = pdf([0.2, 0.1, 0.4, 0.5], cov1, my1)
+K = klassa(D2,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3);
+for i = 1: 75
+    if(K(i) ~= D2c(i,1))
+        disp(K(i))
+        disp(D2c(i,:))
+    end
+end
 
+function klassifiering = klassa(data,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3)
+    klassifiering = zeros(size(data, 1),1)
+    for i = 1: size(data, 1)
+        sann1 = pdf(data(i,:)', cov1, my1)*p1;
+        sann2 = pdf(data(i,:)', cov2, my2)*p2;
+        sann3 = pdf(data(i,:)', cov3, my3)*p3;
+        if sann1 >= sann2 & sann1 >= sann3
+            klassifiering(i) = 1;
+        end
+        if sann2 >= sann1 & sann2 >= sann3
+            klassifiering(i) = 2;
+        end
+        if sann3 >= sann2 & sann3 >= sann1
+            klassifiering(i) = 3;
+        end
+    end
+end
 function [D1,D1c,D2,D2c] = select(features, class)
 
     vect = randsample(150, 75);
@@ -23,6 +47,24 @@ function [D1,D1c,D2,D2c] = select(features, class)
     end
 end
 
+function [p1,p2,p3] = pvarden(data, dataClass)
+    p1=0;p2=0;p3=0;
+    for i = 1: size(data,1)
+        if dataClass(i,1) == 1
+            p1 = p1+1;
+        end
+        if dataClass(i,1) == 2
+            p2 = p2 +1;
+        end
+        if dataClass(i,1) == 3
+            p3 = p3 +1;
+        end
+    end
+    p1 = p1/size(data,1);
+    p2 = p2/size(data,1);
+    p3 = p3/size(data,1);
+end
+
 function a = pdf(x, covm, my)
-    a = (1/((2*pi)^(2)*sqrt(det(covm))))*exp((-1/2)*(x-my)'*(covm)^(-1)*(x-my));
+    a = (1/((2*pi)^(2)*sqrt(det(covm))))*exp((-1/2)*(x-my)'*inv(covm)*(x-my));
 end
