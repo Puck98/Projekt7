b778ce3891cc21caf4a4d38be403a30c277b14da
Jacob Westberg <jawes70@gmail.com>
2021-05-14 11:41:46 +0200

    Testar även en medelvärdeskovariansmatris, lagt till masstest

--- projekt.m
+++ projekt.m
@@ -1,37 +1,73 @@
-%load('Bakad_Data.mat')
-
+if ~(exist("features")) | ~exist("class")
+    load('irisdata.mat')
+end
+disp("-----------Väljer ny data!-----------")
 [D1, D1c, D2, D2c] = select(features, class);
 
 [cov1,cov2,cov3] = kovarians(D1,D1c);
+medKov = mean(cat(3,cov1, cov2, cov3),3);
 [my1,my2,my3] = vantevarde(D1, D1c);
 [p1,p2,p3] = pvarden(D1,D1c);
 
-K = klassa(features,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3);
-for i = 1: size(K,1)
-    if(K(i) ~= class(i))
-        disp([K(i) class(i) i])
-    end
-end
+disp("3 separata kovariansmatriser")
+[fel, andelFel] = comp(D2,D2c,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3);
+str = "antal fel: " + fel + "   |   andel fel: " + andelFel;
+disp(str)
 
-function klassifiering = klassa(data,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3)
-    klassifiering = zeros(size(data, 1),1);
+disp("En genomsnittlig kovariansmatris")
+[fel, andelFel] = comp(D2,D2c,medKov,medKov,medKov,my1,my2,my3,p1,p2,p3);
+str = "antal fel: " + fel + "   |   andel fel: " + andelFel;
+disp(str)
+
+disp("-------------1000 körningar med 'ny' data genomsnitt!------------------")
+fel = zeros(1000,2);
+felmed = zeros(1000,2);
+for i = 1: size(fel,1)
+    [D1, D1c, D2, D2c] = select(features, class);
+    [cov1,cov2,cov3] = kovarians(D1,D1c);
+    medKov = mean(cat(3,cov1, cov2, cov3),3);
+    [my1,my2,my3] = vantevarde(D1, D1c);
+    [p1,p2,p3] = pvarden(D1,D1c);
+    
+    [fel(i,1), fel(i,2)] = comp(D2,D2c,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3);
+    [felmed(i,1), felmed(i,2)] = comp(D2,D2c,medKov,medKov,medKov,my1,my2,my3,p1,p2,p3);
+end
+disp("---separata kovariansmatriser---")
+str = "Genomsnitt antal fel: " + mean(fel(:,1)) + "   |   Genomsnitt andel fel: " + mean(fel(:,2));
+disp(str)
+disp("---Medelvärd kovariansmatris---")
+str = "Genomsnitt antal fel: " + mean(felmed(:,1)) + "   |   Genomsnitt andel fel: " + mean(felmed(:,2));
+disp(str)
+
+function [fel, relFel] = comp(data,dataClass,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3)
+    K = klassa(data,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3);
+    fel = 0;
+    for i = 1: size(K,1)
+        if(K(i) ~= dataClass(i))
+            %disp([K(i) dataClass(i) i])
+            fel = fel + 1;
+        end
+    end
+    relFel = fel/size(data,1);
+end
+function klassificering = klassa(data,cov1,cov2,cov3,my1,my2,my3,p1,p2,p3)
+    klassificering = zeros(size(data, 1),1);
     for i = 1: size(data, 1)
         sann1 = pdf(data(i,:)', cov1, my1)*p1;
         sann2 = pdf(data(i,:)', cov2, my2)*p2;
         sann3 = pdf(data(i,:)', cov3, my3)*p3;
-        if sann1 >= sann2 & sann1 >= sann3
-            klassifiering(i) = 1;
+        if sann1 >= sann2 && sann1 >= sann3
+            klassificering(i) = 1;
         end
-        if sann2 >= sann1 & sann2 >= sann3
-            klassifiering(i) = 2;
+        if sann2 >= sann1 && sann2 >= sann3
+            klassificering(i) = 2;
         end
-        if sann3 >= sann2 & sann3 >= sann1
-            klassifiering(i) = 3;
+        if sann3 >= sann2 && sann3 >= sann1
+            klassificering(i) = 3;
         end
     end
 end
 function [D1,D1c,D2,D2c] = select(features, class)
-
     vect = randsample(150, 75);
     D1 = [];
     D1c = [];
